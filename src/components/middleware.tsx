05b3b8009d8c0f07acb471418c938ef97a1fdbd0
Björn Büttner <Idrinth@users.noreply.github.com>
2024-05-11 19:22:47 +0200

    Add more explanations
    fixes idrinth-api-bench/issues#98

--- src/components/middleware.tsx
+++ src/components/middleware.tsx
@@ -12,6 +12,12 @@ const Middleware = ({
       <Lang lnkey={`middlewares.provided.${ children }.title` as languageKey}/>
     </h3>
     <p>
+      <strong>
+        <Lang lnkey={'middlewares.provided.key'}/>
+      </strong>
+      <pre>^{children}</pre>
+    </p>
+    <p>
       <Lang lnkey={`middlewares.provided.${ children }.description` as languageKey}/>
     </p>
     <h4>
