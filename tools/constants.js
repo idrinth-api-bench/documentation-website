9a32262116222bf75a3443b171d6153d47ff321a
Björn Büttner <Idrinth@users.noreply.github.com>
2024-05-20 05:51:17 +0200

    fix(ci): repair ci
    fix naming
    remove path prefix
    add missing dependencies
    
    fixes idrinth-api-bench/issues#1072

--- tools/constants.js
+++ tools/constants.js
@@ -2,3 +2,7 @@ export const EXIT_FAILURE = 1;
 export const CONTRIBUTOR_PAGE_SIZE = 30;
 const ORGANIZATION_NAME = 'Idrinth-api-bench';
 export const ORGANIZATION_REPOS_URL = `https://api.github.com/orgs/${ ORGANIZATION_NAME }/repos`;
+export const EXIT_SUCCESS = 0;
+export const ERROR_FTA_SCORE = 60;
+export const WARNING_FTA_SCORE = 60;
+export const PADDING_DEFAULT = 1;
