f8715320fcaae91694e7a97132e1ce91eb273b85
Björn Büttner <Idrinth@users.noreply.github.com>
2024-06-04 22:48:51 +0200

    feature: introduce cdn
    fixes #0

--- tools/add-url-in-prod.js
+++ tools/add-url-in-prod.js
@@ -3,15 +3,14 @@ import {
   writeFileSync,
 } from 'fs';
 
-if (process.env.LIVE_SITE !== 'true') {
-  return;
+if (process.env.LIVE_SITE === 'true') {
+  writeFileSync(
+    './dist/index.html',
+    readFileSync('./dist/index.html', 'utf8',)
+      .replace(
+        /<meta charset=UTF-8>/ug,
+        '<meta charset=UTF-8><link rel=base href=https://iab-cdn.com/>',
+      ),
+    'utf8',
+  );
 }
-writeFileSync(
-  './dist/index.html',
-  readFileSync('./dist/index.html', 'utf8',)
-    .replace(
-      /<meta charset=UTF-8>/ug,
-      '<meta charset=UTF-8><link rel=base href=https://iab-cdn.com/>',
-    ),
-  'utf8',
-);
