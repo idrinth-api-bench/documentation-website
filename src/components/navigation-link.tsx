ac269b0124204e14fe08983bf6e95a9de82741b4
Björn Büttner <Idrinth@users.noreply.github.com>
2024-06-07 05:34:36 +0200

    bug: internal links stay internal
    fixes #0

--- src/components/navigation-link.tsx
+++ src/components/navigation-link.tsx
@@ -1,36 +1,5 @@
-import React, {
-  ReactNode,
-} from 'react';
 import {
   NavLink,
-  NavLinkProps,
 } from 'react-router-dom';
 
-const onLoad = () => {
-  const links = document.getElementsByTagName('a',);
-  for (let i = 0; i < links.length; i ++) {
-    const link = links[i];
-    if (link && link.host !== location.hostname) {
-      link.host = location.hostname;
-    }
-  }
-};
-
-window.addEventListener('beforeunload', () => {
-  console.log(window.location.hostname,);
-},);
-
-const NavigationLink = ({
-  to,
-  children,
-  ...props
-}: {
-  to: string,
-  children: string|ReactNode,
-}&NavLinkProps,) => <NavLink
-  {...props}
-  to={to}
-  onLoad={onLoad}
->{children}</NavLink>;
-
-export default NavigationLink;
+export default NavLink;
