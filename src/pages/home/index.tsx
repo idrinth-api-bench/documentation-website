f7dc17acda5d319a9ded1933c32fdeb5072b0fec
Björn Büttner <Idrinth@users.noreply.github.com>
2024-07-06 23:32:33 +0200

    fix: cookie consent for event calendar

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../../components/layout.tsx';
 import ContentUnit from '../../components/content-unit.tsx';
 import Youtube from '../../components/youtube.tsx';
-import Lang from "../../components/lang.tsx";
+import Calendar from "../../components/calendar.tsx";
 
 const Index = () => <Layout
   page='home'
@@ -29,16 +29,9 @@ const Index = () => <Layout
     home.libraries.reusability
     home.libraries.threading
   </ContentUnit>
-  <div className="card">
-    <h2>
-      <Lang lnkey='home.calendar'/>
-    </h2>
-    <iframe
-      style={{borderWidth: 0}}
-      height="500px"
-      width="100%"
-      src="https://calendar.online/3c3632e38202b4bf4ec5?iframe=true">
-    </iframe>
-  </div>
+  <Calendar
+    title={'home.calendar'}
+    link={'04ca1b740ec95ed69088'}
+  >3c3632e38202b4bf4ec5</Calendar>
 </Layout>;
 export default Index;
